0b8e9fc7b90fe3f17cb3930fb70b5a85ece015e8
Ishwari <159119151+IshwariDole@users.noreply.github.com>
2026-07-07 17:21:59 +0000

    added binarysearch

---
+++ BinarySearch.java
@@ -0,0 +1,26 @@
+public class BinarySearch {
+
+    public int binarySearch(int arr[], int key){
+        int low=0;
+        int high=arr.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==key){
+                return mid; 
+            }
+           else if(arr[mid]<key){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+           }
+        return -1;
+     }
+    public static void main(String args[]){
+        int arr[]={10,20,30,40,50};
+        int key=40;
+        System.out.println("Key found at index: "+new BinarySearch().binarySearch(arr,key));
+
+    }
+}
